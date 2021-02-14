5000add23da0d6277d10e7e07db6633d71a94a8f
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-14 16:56:33 +0100

    script for finding PE files

--- PE/pe_finder.py
+++ PE/pe_finder.py
@@ -32,4 +32,27 @@ def process_file(filename):
 				Copies the file to the output directory if it a valid PE file.
 				"""
 				if verbose_mode:
-								print
+								print "Proccesing the fiel %s" % filename
+				try:
+								pe = pefile.PE(filename, fast_load=True)
+
+								parts = filename.split("\\")
+
+								shutil.copyfile(filename, join(output_dirs, parts[-1]))
+
+				except pefile.PEFormatError:
+								if verbose_mode:
+												print "Skipping: file could not be read (probably a 16bit file)"
+
+if not output_dirs:
+				print "Error: Please configure the output directory"
+				sys.exit(0)
+
+for input_dirs, do_walk:
+				for root, dirs, files in os.walk(input_dirs):
+								for file i [ file for file in files if file_qualifies(file.lower(), input_extensions								)]:
+												procces_file(join(root, file))
+								else:
+												for file in [file for file in os.listdir(input_dir) if file_qualifies(file.l												ower(), input_extensions)]:
+																process_file(input_dir, file))
+
